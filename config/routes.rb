8d8475ba118bfae726578adae29c7229808b81dd
muirisc <91299876+muirisc@users.noreply.github.com>
2022-01-11 12:56:47 -0500

    commit tree 94aad4f51988cfd2a94390a2c0fe45037502d2b7

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants, only: [:index, :show, :create, :update]
+  get "/plants", to: "plants#index"
+  get "/plants/:id", to: "plants#show"
+  patch "/plants/:id", to: "plants#update"
+  delete "/plants/:id", to: "plants#delete"
 end
